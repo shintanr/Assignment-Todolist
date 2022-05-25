8807bdf226511458075e2bac97b2ecbee032dfbf
shintanr <shintanurrohmah143@gmail.com>
2022-05-26 03:13:30 +0700

    update desain

--- AssignmentTodo.php
+++ AssignmentTodo.php
@@ -34,30 +34,43 @@ if(isset($_GET['delete'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ASSIGNMENT TODOLIST</title>
+    <link rel="stylesheet" href="style.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
 </head>
-<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#"> <i class="bi bi-bag-fill"></i>TO DO</a>
+        </div>
+    </nav>
+    <!-- Akhir Navbar -->
+    <div class="jumbotron bg-light">
+    <h1 class="display-2 text-center " padding="5rem" >ASSIGNMENT TODOLIST</h1>
+    </div>
+       
+    <!-- akhir jumbotron -->
+        <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="text-center">ASSIGNMENT TODOLIST</h1>
+                <h1 class="text-center"></h1>
             </div>
         </div>
         <div class="row">
             <div class="col-md-6">
                 <div class="card shadow-lg border-0">
                     <div class="card-body">
-                        <h3>Form Tambah Tugas</h3>
+                        <h3>What's your task?</h3>
                         <form method="post">
                         <div class="form-group">
-                            <input type="text" class="form-control" name="task_name" placeholder="input nama tugas">
+                            <input type="text" class="form-control" name="task_name" placeholder="input task">
                         </div>
                         <div class="form-group">
-                            <button type="submit" name="add_post" class="btn btn-primary btn-block">Tambah Tugas</button>
+                            <button type="submit" name="add_post" class="btn btn-secondary btn-block">Add Task</button>
                         </div>
                         </form>
-                        <h3>List Pending Tugas</h3>
+                        <h3>Pending Tasks</h3>
                         <ul class="list-group">
                             <?php
                             $query = mysqli_query($connection, "SELECT * FROM tasks WHERE task_status = 'pending' ");
@@ -68,12 +81,12 @@ if(isset($_GET['delete'])){
                             <li class="list-group-item">
                                 <?php echo $task_name; ?>
                                 <div class="float-right">
-                                    <a href="AssignmentTodo.php?edit= <?php echo $task_id ?>" class="btn btn-info">
+                                    <a href="AssignmentTodo.php?edit= <?php echo $task_id ?>" class="btn btn-info" data-toggle="tooltip" data-placement="top" title="add todo">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
                                 <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
                                 </svg>
                                 </a>    
-                                                                                                    <a href="AssignmentTodo.php?delete= <?php echo $task_id ?>" class="btn btn-danger">
+                                                                                                    <a href="AssignmentTodo.php?delete= <?php echo $task_id ?>" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="remove todo">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -89,7 +102,7 @@ if(isset($_GET['delete'])){
             <div class="col-md-6">
                 <div class="card shadow-lg border-0">
                     <div class="card-body">
-                        <h3>List Tugas Selesai</h3>
+                        <h3>Hurray, done!</h3>
                         <ul class="list-group">
                             <?php 
                             $query = mysqli_query($connection, "SELECT * FROM tasks WHERE task_status = 'selesai' ");
@@ -99,7 +112,7 @@ if(isset($_GET['delete'])){
                             <?php echo $row['task_name']?>
 
                             <div class="float-right">
-                                    <span class="badge badge-primary" <?php echo $row['task_status'] ?>>selesai</span>
+                                    <span class="badge badge-warning" <?php echo $row['task_status'] ?>>selesai</span>
                             </div>
                             </li>
                             <?php } ?>
@@ -109,8 +122,23 @@ if(isset($_GET['delete'])){
             </div>
         </div>
     </div>
+
+
     
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-</body>
+    <script>$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    })</script>
+    
+    </body>
+
+    <!-- footer -->
+    <footer class="bg-dark text-white text-center pb-5 fixed-bottom">
+        <p>Created with <3 <i class="bi bi-heart-fill"></i> by
+            <a href="https://github.com/shintanr" class="text-white fw-bold" target="_blank"><u>Shinta Nurrohmah</u></a>
+        </p>
+    </footer>
+    <!-- Akhir footer -->
+
 </html>
