d196d1925ebfae40b20ec039e92229f475c07647
shintanr <shintanurrohmah143@gmail.com>
2022-05-26 03:32:00 +0700

    update comment

--- AssignmentTodo.php
+++ AssignmentTodo.php
@@ -22,8 +22,6 @@ if(isset($_GET['delete'])){
     $query = mysqli_query($connection, "DELETE FROM tasks WHERE task_id = '$task_id'");
     header("Location: AssignmentTodo.php");
 }
-
-
 ?>
 
 
@@ -51,7 +49,9 @@ if(isset($_GET['delete'])){
     <h1 class="display-2 text-center " padding="5rem" >ASSIGNMENT TODOLIST</h1>
     </div>
     <!-- akhir jumbotron -->
-        <body>
+
+
+    <body>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -87,7 +87,7 @@ if(isset($_GET['delete'])){
                                 <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
                                 </svg>
                                 </a>    
-                                                                                                    <a href="AssignmentTodo.php?delete= <?php echo $task_id ?>" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="remove todo">
+                                <a href="AssignmentTodo.php?delete= <?php echo $task_id ?>" class="btn btn-danger" data-toggle="tooltip" data-placement="top" title="remove todo">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
@@ -130,15 +130,16 @@ if(isset($_GET['delete'])){
     <script>$(function () {
     $('[data-toggle="tooltip"]').tooltip()
     })</script>
+</body>
+<!-- Akhir body -->
     
-    </body>
-
-    
+<!-- Footer -->
     <footer class="bg-dark text-white text-center pb-5 fixed-bottom">
         <p>Created with <3 <i class="bi bi-heart-fill"></i> by
             <a href="https://github.com/shintanr" class="text-white fw-bold" target="_blank"><u>Shinta Nurrohmah</u></a>
         </p>
     </footer>
+<!-- Akhir Footer -->
    
 
 </html>
